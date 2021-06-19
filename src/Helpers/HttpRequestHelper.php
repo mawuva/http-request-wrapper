c49cc23dc8423f0fe4e54341390bacbf8b14aad6
Seddor Ephraïm <seddorephraim7@gmail.com>
2021-06-19 11:39:45 +0200

    Add get, post, put, patch and delete methods in HttpRequestHelper.php

--- src/Helpers/HttpRequestHelper.php
+++ src/Helpers/HttpRequestHelper.php
@@ -9,22 +9,100 @@ trait HttpRequestHelper
     /**
      * Undocumented function
      *
-     * @param [type] $method
-     * @param [type] $requestUrl
-     * @param [type] $options
+     * @param string $method
+     * @param string $requestUrl
+     * @param array $options
      *
-     * @return void
+     * @return mixed
      */
-    public function makeRequest($method, $requestUrl, $options)
+    public function makeRequest($method, $requestUrl, $options = [])
     {
-        if (property_exists($this, 'baseUri')) {
-            $request = new HttpRequest($this ->baseUri);
-        }
+        return $this ->buildRequest() 
+                     ->send($method, $requestUrl, $options);
+    }
 
-        $request = property_exists($this, 'baseUri') 
-                    ? new HttpRequest($this ->baseUri) 
-                    : new HttpRequest();
+    /**
+     * Perform GET request
+     *
+     * @param string $requestUrl
+     * @param array $options
+     *
+     * @return mixed
+     */
+    public function get($requestUrl, $options = [])
+    {
+        return $this ->buildRequest() 
+                     ->get($requestUrl, $options);
+    }
 
-        return $request ->send($method, $requestUrl, $options);
+    /**
+     * Perform POST request
+     *
+     * @param string $requestUrl
+     * @param array $formParams
+     * @param array $options
+     *
+     * @return mixed
+     */
+    public function post($requestUrl, $formParams = [], $options = [])
+    {
+        return $this ->buildRequest() 
+                     ->post($requestUrl, $formParams, $options);
+    }
+
+    /**
+     * Perform PUT request
+     *
+     * @param string $requestUrl
+     * @param array $formParams
+     * @param array $options
+     *
+     * @return mixed
+     */
+    public function put($requestUrl, $formParams = [], $options = [])
+    {
+        return $this ->buildRequest() 
+                     ->put($requestUrl, $formParams, $options);
+    }
+
+    /**
+     * Perform PATCH request
+     *
+     * @param string $requestUrl
+     * @param array $formParams
+     * @param array $options
+     *
+     * @return mixed
+     */
+    public function patch($requestUrl, $formParams = [], $options = [])
+    {
+        return $this ->buildRequest() 
+                     ->patch($requestUrl, $formParams, $options);
+    }
+
+    /**
+     * Perform DELETE request
+     *
+     * @param string $requestUrl
+     * @param array $options
+     *
+     * @return mixed
+     */
+    public function delete($requestUrl, $options = [])
+    {
+        return $this ->buildRequest() 
+                     ->delete($requestUrl, $options);
+    }
+
+    /**
+     * Instantiate HTTP Request class
+     * 
+     * @return Mawuekom\HttpRequestWrapper\HttpRequest
+     */
+    private function buildRequest()
+    {
+        return property_exists($this, 'baseUri') 
+                ? new HttpRequest($this ->baseUri) 
+                : new HttpRequest();
     }
 }
